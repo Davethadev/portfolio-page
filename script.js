5135383f1dce5026f81781e24e11568879a05a31
David Umanah <david_umanah@yahoo.com>
2022-09-05 00:19:02 +0100

    improve style, add projects

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon')
 const headerLinksMobile = document.querySelector('.header-links-mobile')
+const mobileLinks = document.querySelectorAll('.mobile-link')
+const buttons = document.querySelectorAll('button')
+
+
 
 hamburgerIcon.addEventListener('click', function() {
     headerLinksMobile.classList.toggle('active')
+})
+
+mobileLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        headerLinksMobile.classList.toggle('active')
+    })
+})
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        headerLinksMobile.classList.toggle('active')
+    })
 })
